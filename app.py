9f10387bc3886f5ffa97fadbb71da7118d05a372
Joham Chouhan <johamchouhan@gmail.com>
2025-12-29 16:38:25 +0530

    Fun Fair Project

---
+++ app.py
@@ -0,0 +1,46 @@
+from flask import Flask, render_template, request, redirect, url_for
+from models import Player, db
+
+app = Flask ( __name__ )
+app.config [ "SQLALCHEMY_DATABASE_URI" ] = "sqlite:///fun_fair.db"
+db.init_app ( app )
+
+with app.app_context ( ):
+	db.create_all ( )
+
+@app.route ( "/", methods = [ "GET", "POST" ] )
+def index ( ):
+	if request.method == "POST":
+		first_name = request.form.get ( "first_name" )
+		last_name = request.form.get ( "last_name" )
+		contact_number = request.form.get ( "contact_number" )
+		email = request.form.get ( "email" )
+		payment_mode = "pending"
+		new_player = Player ( first_name = first_name, last_name = last_name, phone = contact_number, email = email, marks = 0, payment_mode = payment_mode )
+		db.session.add ( new_player )
+		db.session.commit ( )
+		return redirect ( url_for ( 'leader_board' ))
+	return render_template ( "index.html" )
+
+@app.route ( "/leader_board", methods = [ "GET", "POST" ] )
+def leader_board ( ):
+	all_players = Player.query.order_by ( Player.marks.desc ( )).all ( )
+	return render_template ( "leader_board.html", all_players = all_players )
+
+@app.route ( "/update/payment_mode/<int:player_id>/<mode>", methods = [ "GET", "POST" ] )
+def update_payment_mode ( player_id, mode ):
+	player = Player.query.get ( player_id )
+	player.payment_mode = mode
+	db.session.commit ( )
+	return redirect ( url_for ( 'leader_board' ))
+
+@app.route ( "/update_marks/<int:player_id>", methods = [ "GET", "POST" ] )
+def update_marks ( player_id ):
+	player = Player.query.get ( player_id )
+	marks = request.form.get ( "marks" )
+	player.marks = marks
+	db.session.commit ( )
+	return redirect ( url_for ( "leader_board" ))
+
+if __name__ == "__main__":
+	app.run ( debug = True )
---
+++ models.py
@@ -0,0 +1,13 @@
+from flask_sqlalchemy import SQLAlchemy
+
+
+db = SQLAlchemy ( )
+
+class Player ( db.Model ):
+	id = db.Column ( db.Integer, primary_key = True )
+	first_name = db.Column ( db.String ( 20 ), nullable = False )
+	last_name = db.Column ( db.String ( 20 ), nullable = False )
+	phone = db.Column ( db.Integer )
+	email = db.Column ( db.String ( 40 ) )
+	marks = db.Column ( db.Integer )
+	payment_mode = db.Column ( db.String ( 10 ) )
